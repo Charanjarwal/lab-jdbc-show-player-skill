bac84c54182dd6ed3e5029d3321a1447c0ddb60a
charanjarwal <charanjarwal@LAPTOP-O72MBBSG.localdomain>
2020-09-16 15:20:33 +0530

    done

--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,36 @@
+package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO{
+	
+	public List<Skill> listAllSkills () throws ClassNotFoundException, SQLException {
+
+		String sql="select * from skill";
+		
+		PreparedStatement ps=ConnectionManager.getConnection().prepareStatement(sql);
+		
+		ResultSet rs=ps.executeQuery();
+
+		
+		while(rs.next()) {
+			// Retrieve by column name and displaying
+			long skillId=rs.getLong(1);
+			String skillname=rs.getString(2);
+			Skill s=new Skill(skillId,skillname);
+			
+			
+			System.out.println(skillname);
+			System.out.println(skillId);
+		}
+		ConnectionManager.getConnection().close();
+		return null;
+		
+	}
+}
